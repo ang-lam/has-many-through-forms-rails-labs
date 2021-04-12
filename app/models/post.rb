6d520745fe1b4f8b0f98ac5b5d259d5ecd10b3e0
Angela <angelalamluu@gmail.com>
2021-04-12 00:23:01 -0700

    commit tree 10190d34bdb137146b98d067268573a29936542d

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,15 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  def categories_attributes=(categories_attributes)
+    categories_attributes.values.each do |category|
+      if !category[:name].empty?
+      # don't want bad data so only do this if user entered or selected data
+      # can also do if category[:name].present?
+        self.categories << Category.find_or_create_by(name: category[:name])
+      end
+    end
+  end
+
 
 end
